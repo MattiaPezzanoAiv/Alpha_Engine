2bfda5f94dc3bebbfba1e09dddf8f25a7c36875b
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2017-07-28 20:00:06 +0200

    added object classes and basic interfaces

---
+++ AlphaEngine/Engine/GameObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaEngine
+{
+    public class GameObject :BaseObject, IStartable, IUpdatable
+    {
+        private bool started;
+
+        public GameObject(string name) //go must be have a name, it's his unique identifier
+        {
+            this.Name = name;
+
+        }
+
+
+        public void Start()
+        {
+            //use this for initialization?
+            started = true;
+        }
+
+        public void Update()
+        {
+            if (!started) Start();
+        }
+    }
+}
