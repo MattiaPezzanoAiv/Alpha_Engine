ede59cd910dc5911640ea397abf9be856b10b1ac
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2017-07-28 21:44:17 +0200

    updated project with new tests on gameobject and various bug fix

---
+++ AlphaEngine.Test/TestGameObject.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Aiv.Fast2D;
+using OpenTK;
+
+namespace AlphaEngine.Test
+{
+    [TestFixture]
+    public class TestGameObject
+    {
+        public class FakeComponent:Component
+        {
+            
+        }
+        public class FakeComponent2 : Component
+        {
+
+        }
+
+        private GameObject go;
+
+        [SetUp]
+        public void Init()
+        {
+            AlphaEngine.Init("test", new Vector2(50, 50));
+            go = new GameObject("TEST");
+        }
+
+        [Test]
+        public void GreenLight_NameAssignation()
+        {
+            Assert.That(go.Name, Is.EqualTo("TEST"));
+        }
+        [Test]
+        public void RedLight_NameAssignation()
+        {
+            go.Name = "RedLight";
+            Assert.That(go.Name, Is.EqualTo("RedLight"));
+        }
+
+        [Test]
+        public void GreenLight_AddComponentAndGetComponent()
+        {
+            FakeComponent fake = go.AddComponent<FakeComponent>();
+            Assert.That(go.GetComponent<FakeComponent>(), Is.EqualTo(fake));
+        }
+        [Test]
+        public void RedLight_AddComponentAndGetComponent()
+        {
+            FakeComponent2 fake = go.AddComponent<FakeComponent2>();
+            Assert.That(go.GetComponent<FakeComponent>(), Is.EqualTo(null));
+        }
+        [Test]
+        public void Test_MultipleAddComponent()
+        {
+            FakeComponent2 fake = go.AddComponent<FakeComponent2>();
+            FakeComponent2 fake2 = go.AddComponent<FakeComponent2>();
+            FakeComponent2 fake3 = go.AddComponent<FakeComponent2>();
+            Assert.That(go.GetComponent<FakeComponent2>(), Is.EqualTo(fake)); //list is ordered
+        }
+        [Test]
+        public void Test_TryToUseSetOwnerManually()
+        {
+            GameObject go1 = new GameObject("mario");
+            GameObject go2 = new GameObject("paolo");
+
+            FakeComponent c = go1.AddComponent<FakeComponent>();
+            Assert.That(c.GameObject, Is.EqualTo(go1));
+            c.SetOwner(go2);  //will be ignored
+            Assert.That(c.GameObject, Is.EqualTo(go1));
+        }
+    }
+}
--- AlphaEngine/Engine/GameObject.cs
+++ AlphaEngine/Engine/GameObject.cs
@@ -6,20 +6,23 @@ using System.Threading.Tasks;
 
 namespace AlphaEngine
 {
-    public class GameObject :BaseObject, IStartable, IUpdatable
+    public sealed class GameObject :BaseObject, IStartable, IUpdatable
     {
         private bool started;
+        private List<Component> components;
+
 
         public GameObject(string name) //go must be have a name, it's his unique identifier
         {
             this.Name = name;
-
+            components = new List<Component>();
         }
 
 
         public void Start()
         {
             //use this for initialization?
+       
             started = true;
         }
 
@@ -27,5 +30,35 @@ namespace AlphaEngine
         {
             if (!started) Start();
         }
+
+        /// <summary>
+        /// Add a component object (or inherited) to a selected gameobject
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>Return the instance of added component</returns>
+        public T AddComponent<T>() where T : Component,new()
+        {
+            T newComp = new T();
+            newComp.SetOwner(this);
+            components.Add(newComp);
+            return newComp;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>Return the first component of type T if is present, else return null</returns>
+        public T GetComponent<T>() where T : Component
+        {
+            foreach (Component c in components)
+            {
+                if (c is T)
+                    return (T)c;
+            }
+            return null;
+        }
+
+        
     }
 }
