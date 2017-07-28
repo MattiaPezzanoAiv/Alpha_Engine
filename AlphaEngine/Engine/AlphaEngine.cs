47d74532c076f8ddd027091b4d3429e857b63730
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2017-07-28 18:56:53 +0200

    added project

---
+++ AlphaEngine/Engine/AlphaEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Fast2D;
+
+namespace AlphaEngine
+{
+    public static class AlphaEngine
+    {
+        private static Window window;
+        private static Time time;
+
+    }
+}
