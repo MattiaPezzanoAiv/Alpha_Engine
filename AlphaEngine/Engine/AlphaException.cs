398ec970518dc26a6e3c9612c1a731422b097e5d
MattiaPezzanoAiv <mattiapezzano7@gmail.com>
2017-07-30 21:50:59 +0200

    added exception file

---
+++ AlphaEngine/Engine/AlphaException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaEngine
+{
+    public class AlphaException:Exception
+    {
+        public AlphaException(string message)
+        {
+
+        }
+        public AlphaException()
+        {
+
+        }
+    }
+    public class NotAComponentException : AlphaException
+    {
+
+    }
+}
